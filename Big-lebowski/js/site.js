278630b4b85b2c0207ee8add700a1d3b412fd4fc
Liutong Chen <liutong.clt@gmail.com>
2017-06-01 16:09:51 -0700

    finish modifying

--- Big-lebowski/js/site.js
+++ Big-lebowski/js/site.js
@@ -6,4 +6,26 @@
         let reason = $(this).text();
         $pickBtn.text(reason);
     })
+
+    let $sentDialog = $('#sentDialog');
+    $('#contactForm').on("submit", function() {
+        $sentDialog.modal("show");
+        return false;
+    })
+
+    let $sentAlert = $("#sentAlert");
+    $sentDialog.on("hidden.bs.modal", function() {
+        $sentAlert.show();
+    })
+
+    $sentAlert.on("close.bs.alert", function() {
+        $sentAlert.hide();
+        return false; 
+    })
+    $("#contactForm input[type=submit]").tooltip({
+        show: 500,
+        hide:0
+    });
+
+    $("#theCarousel").carousel()
 })();
